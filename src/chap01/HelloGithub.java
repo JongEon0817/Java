325ad890d9c2d85ae5ac7225bc3fd55b9757e20c
JongEon0817 <kimje0817@gmail.com>
2019-07-12 19:54:03 +0900

    메시지 추가

--- src/chap01/HelloGithub.java
+++ src/chap01/HelloGithub.java
@@ -5,6 +5,7 @@ public class HelloGithub {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World and GitHub");
+		System.out.println("추가 내용 삽입 후 변화과정 관망!!!");
 	}
 
 }
